ba40337598afe8b4a76c610230ac13a8cc822918
AhmedHosny1998 <hosny4620Qgmail.com>
2023-11-12 07:50:29 -0800

    dfgd

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    x=list(map(lambda submat:list(map(lambda e:e**2,submat)),matrix))
-    print(x)
+    return list(map(lambda submat:list(map(lambda e:e**2,submat)),matrix))
