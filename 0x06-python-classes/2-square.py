6bce229a0dbabf271c544ac2ece3c5302fe48c22
AhmedHosny1998 <hosny4620Qgmail.com>
2023-11-22 15:25:21 -0800

    gfd

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+2. Size validation
+A class Square that defines a square
+"""
+
+
 class Square:
     """Class that defines a square"""
 
@@ -8,21 +14,10 @@ class Square:
         Raise TypeError & ValueError if not int or <0 resp.
         """
 
-        if not isinstance(size ,int):
+        if not isinstance(size, int):
             raise TypeError('size must be an integer')
 
         if size < 0:
             raise ValueError('size must be >= 0')
 
         self.__size = size
-        
-    def area(self):
-        """Area of this aquare
-
-
-        returns:
-        the size square"""
-
-        return self.__size ** 2
-        
-
