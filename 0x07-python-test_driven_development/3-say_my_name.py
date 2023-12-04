15db3d22afade0bdab19c5562c10af7bd1da3f59
AhmedHosny1998 <hosny4620Qgmail.com>
2023-12-03 19:11:22 -0800

    dfg

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,14 @@
+
 #!/usr/bin/python3
 """Defines a function that prints my name.
 
 Attributes:
     say_my_name: function that prints my name.
 """
+
+
 def say_my_name(first_name, last_name=""):
-"""Prints my name.
+    """Prints my name.
 
     Args:
         first_name (str): My first name.
@@ -14,14 +17,10 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: If either first name or last name is not a string.
     """
-
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
+
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+
     print("My name is {} {}".format(first_name, last_name))
-if __name__ == '__main__':
-    import doctest
-
-    doctest.testfile("tests/3-say_my_name.txt")
-
