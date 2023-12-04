f0567777119c268e4beb068fd7b41e47c88a7d99
AhmedHosny1998 <hosny4620Qgmail.com>
2023-12-03 21:03:43 -0800

    fd

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -2,27 +2,30 @@
 """Defines a function that prints a square with the character #.
 
 Attributes:
-    print_square: function that prints a square with the character #.
-"""
+        print_square: function that prints a square with the character #.
+        """
 
 
-def print_square(size):
-   """Prints a square with the character #.
+        def print_square(size):
+                """Prints a square with the character #.
 
-    Args:
-        size (int): Size of the square (1 side).
+                    Args:
+                            size (int): Size of the square (1 side).
 
-    Raises:
-        TypeError: If size is not an integer and less than 0.
-        ValueError: If size is less than 0.
-    """
+                                Raises:
+                                        TypeError: If size is not an integer and less than 0.
+                                                ValueError: If size is less than 0.
+                                                    """
 
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
 
-    for x in range(size):
-        print("#" * size)
+                                                        if not isinstance(size, int):
+                                                                    raise TypeError("size must be an integer")
+
+                                                                    if size < 0:
+                                                                                raise ValueError("size must be >= 0")
+
+                                                                                if isinstance(size, float) and size < 0:
+                                                                                            raise TypeError("size must be an integer")
+
+                                                                                            for i in range(size):
+                                                                                                        print("#" * size)
