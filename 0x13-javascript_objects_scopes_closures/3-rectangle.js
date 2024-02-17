fa52a37cbc2a0abb2bb2804e5fed73c4a3ad9806
AhmedHosny1998 <hosny4620Qgmail.com>
2024-02-17 15:24:16 -0800

    js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
-class Rectangle{
-    constructor (w, h){
-        if ((h > 0) && (w > 0)){
-            this.width = w;
-            this.height = h;
-        }
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print(){
-        for (let i = 0; i < this.height; i++){
-            let s = '';
-            for (let j =0; j < this.width; j++){
-                s += 'X';
-            }
-            console.log(s);
-        }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
     }
-
+  }
 }
-mod.exports = Rectangle;
+
+module.exports = Rectangle;
