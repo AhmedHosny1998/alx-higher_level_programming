11f2a75986c9a5d9ede061b7753413ddc5bb5e3c
AhmedHosny1998 <hosny4620Qgmail.com>
2024-03-15 18:39:44 -0700

    pytohn

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ a Python script that fetches https://alx-intranet.hbtn.io/status
 """
+
+
 if __name__ == '__main__':
-    import urlib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as re:
-        content = re.read()
-        print("Body response : ")
-        print(f"\t- type : {type(content)}")
+    import urllib.request
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
         print(f"\t- content: {content}")
-        print(f"\t- utf8 content : {content.decode('utf-8')}")
+        print(f"\t- utf8 content: {content.decode('utf-8')}")
